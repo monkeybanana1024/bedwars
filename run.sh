f5f1fcf2f7d0a2d0fbe9abf8ecf1485ac9f84891
Arnav Saraf <156858349+monkeybanana1024@users.noreply.github.com>
2026-01-14 22:40:40 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 cd Bedwars
+gh codespace ports visibility 25565:public -c "$CODESPACE_NAME"
 java -javaagent:slimeworldmanager-classmodifier-2.2.1.jar -jar -Xmx2048m -Dfile.encoding=UTF-8 paper-1.12.2.jar
